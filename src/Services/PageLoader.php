f394f7f8aa4b7d7dc422305839d9b39f4d781b45
vbilan <vasil@mage.team>
2025-09-24 20:49:56 +0300

    fixing new format of page

--- src/Services/PageLoader.php
+++ src/Services/PageLoader.php
@@ -2,28 +2,32 @@
 
 namespace App\Services;
 
+use Symfony\Component\DomCrawler\Crawler;
 
 class PageLoader
 {
 
-     public function __construct(private readonly string $cacheDir)
-     {
-     }
-     public function getPage(int $year, int $week)
-     {
-         $dir = $this->cacheDir . '/cached_pages/' . $year;
-         $file = $dir . '/' . $week . '.html';
-         if (!file_exists($dir)) {
-             mkdir($dir, 0777, true);
-         }
-         if (file_exists($file)) {
-             return file_get_contents($file);
-         }
+    public function __construct(private readonly string $cacheDir) {}
+    public function getPage(int $year, int $week)
+    {
+        $dir = $this->cacheDir . '/cached_pages/' . $year;
+        $file = $dir . '/' . $week . '.html';
+        if (!file_exists($dir)) {
+            mkdir($dir, 0777, true);
+        }
+        if (file_exists($file)) {
+            return file_get_contents($file);
+        }
 
-         $url = 'https://wol.jw.org/uk/wol/meetings/r15/lp-k/' . $year. '/' . $week;
+        $url = 'https://wol.jw.org/uk/wol/meetings/r15/lp-k/' . $year . '/' . $week;
 
-         $content =  file_get_contents($url);
-         file_put_contents($file, $content);
-         return $content;
-     }
+        $content =  file_get_contents($url);
+
+        $crawler = new Crawler($content);
+        $href = $crawler->filter('#materialNav a')->first()->attr('href');
+        $url = 'https://wol.jw.org' . $href;
+        $content =  file_get_contents($url);
+        file_put_contents($file, $content);
+        return $content;
+    }
 }
